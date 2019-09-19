5a5e0fb891c6c6362ce1168426018ae7c33fbfec
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-09-18 22:42:33 -0300

    Update run.sh

--- experiment/run.sh
+++ experiment/run.sh
@@ -10,6 +10,9 @@ NC='\033[0m'
 echo "FLAGS: ${FLAGS:=gci nogci}"
 echo "NUMBER_OF_EXPERIMENTS: ${NUMBER_OF_EXPERIMENTS:=8}"
 
+echo -e "${YELLOW}BUILDING UP CONTAINERS${NC}"
+bash build.sh
+
 mkdir -p ./input-entries/
 for expid in `seq 1 ${NUMBER_OF_EXPERIMENTS}`;
 do
@@ -17,9 +20,6 @@ do
     do
         echo -e "${YELLOW}TEARING DOWN CONTAINERS${NC}"
         bash teardown.sh
-
-        echo -e "${YELLOW}BUILDING UP CONTAINERS${NC}"
-        bash build.sh
 
         echo -e "${YELLOW}SETTING UP CONTAINERS${NC}"
         CONTAINER_TAG="${flag}" bash setup.sh
