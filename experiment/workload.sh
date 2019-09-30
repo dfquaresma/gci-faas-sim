15b9dbff6d6e895f8ae0031ac9b28a0578082cac
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-09-30 12:12:17 -0300

    Update workload.sh to receive number of requests as parameters

--- experiment/workload.sh
+++ experiment/workload.sh
@@ -3,15 +3,15 @@ date
 set -x
 
 # To avoid execution without passing environment variables
-if [[ (-z "$FILE_NAME") || (-z "$FUNCTION_TARGET_IP") || (-z "$REPO_PATH") ]];
+if [[ (-z "$NUMBER_OF_REQUESTS") || (-z "$FILE_NAME") || (-z "$FUNCTION_TARGET_IP") || (-z "$REPO_PATH") ]];
 then
-  echo -e "${RED}FILE_NAME AND/OR FUNCTION_TARGET_IP AND/OR REPO_PATH MISSING: workload.sh${NC}"
+  echo -e "${RED}NUMBER_OF_REQUESTS AND/OR FILE_NAME AND/OR FUNCTION_TARGET_IP AND/OR REPO_PATH MISSING: workload.sh${NC}"
   exit
 fi
 
 mkdir -p ${REPO_PATH}
 echo -e "status;latency" > ${FILE_NAME}
-for i in `seq 1 50000`
+for i in `seq 1 ${NUMBER_OF_REQUESTS}`
 do
     curl -X GET -o /dev/null -s -w '%{http_code};%{time_total}\n' ${FUNCTION_TARGET_IP}:8080 >> ${FILE_NAME}
 done
