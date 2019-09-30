f03c34589ac0c584c6b97e3bca7930f5dfbb5c15
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-09-30 12:12:45 -0300

    Update run script

--- experiment/run.sh
+++ experiment/run.sh
@@ -8,7 +8,8 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 echo "FLAGS: ${FLAGS:=gci nogci}"
-echo "NUMBER_OF_EXPERIMENTS: ${NUMBER_OF_EXPERIMENTS:=2}"
+echo "NUMBER_OF_EXPERIMENTS: ${NUMBER_OF_EXPERIMENTS:=1}"
+echo "NUMBER_OF_REQUESTS: ${NUMBER_OF_REQUESTS:=50000}"
 echo "FUNCTION_TARGET_IP: ${FUNCTION_TARGET_IP:=10.11.16.93}"
 echo "WORKLOAD_TARGET_IP: ${WORKLOAD_TARGET_IP:=10.11.16.117}"
 echo "ID_RSA_PATH: ${ID_RSA_PATH:=id_rsa}"
@@ -36,10 +37,10 @@ do
         ssh -i ${ID_RSA_PATH} ubuntu@${FUNCTION_TARGET_IP} -o StrictHostKeyChecking=no "${CD_TO_SCRIPTS_PATH}; sudo CONTAINER_TAG=${flag} bash setup.sh"
 
         echo -e "${RED}RUNNING WORKLOAD FOR ${CONTAINER_TAG} EXPID ${EXPID}${NC}"
-        ssh -i ${ID_RSA_PATH} ubuntu@${WORKLOAD_TARGET_IP} -o StrictHostKeyChecking=no "${CD_TO_SCRIPTS_PATH}; sudo FUNCTION_TARGET_IP=${FUNCTION_TARGET_IP} REPO_PATH=${REPO_PATH} FILE_NAME=${REPO_PATH}${flag}${expid}.csv bash workload.sh"
+        ssh -i ${ID_RSA_PATH} ubuntu@${WORKLOAD_TARGET_IP} -o StrictHostKeyChecking=no "${CD_TO_SCRIPTS_PATH}; sudo NUMBER_OF_REQUESTS=${NUMBER_OF_REQUESTS} FUNCTION_TARGET_IP=${FUNCTION_TARGET_IP} REPO_PATH=${REPO_PATH} FILE_NAME=${REPO_PATH}${flag}${expid}.csv bash workload.sh"
 
         echo -e "${RED}GETTING RESULTS FOR ${CONTAINER_TAG} EXPID ${EXPID}${NC}"
-        ssh -i ${ID_RSA_PATH} ubuntu@${FUNCTION_TARGET_IP} -o StrictHostKeyChecking=no "${CD_TO_SCRIPTS_PATH}; sudo CONTAINER_TAG="${flag}" REPO_PATH=${REPO_PATH} FILE_NAME=${REPO_PATH}${flag}${expid}.log bash getlogs.sh"
+        ssh -i ${ID_RSA_PATH} ubuntu@${FUNCTION_TARGET_IP} -o StrictHostKeyChecking=no "${CD_TO_SCRIPTS_PATH}; sudo CONTAINER_TAG="${flag}" REPO_PATH=${REPO_PATH} FILE_NAME=${REPO_PATH}${flag}${expid} bash getlogs.sh"
     done;
 done
 
