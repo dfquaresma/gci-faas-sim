cec6ede5a05aafbdb110e85d4deb3d25771491f4
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-10-07 18:06:40 -0300

    Update workload script to easier debugging

--- experiment/workload.sh
+++ experiment/workload.sh
@@ -12,7 +12,9 @@ fi
 echo -e "status;latency" > ${FILE_NAME}
 for i in `seq 1 ${NUMBER_OF_REQUESTS}`
 do
-    curl -X GET -o /dev/null -s -w '%{http_code};%{time_total}\n' ${FUNCTION_TARGET_IP}:8080 >> ${FILE_NAME}
+    curl_return=$(curl -X GET -o /dev/null -s -w '%{http_code};%{time_total}\n' ${FUNCTION_TARGET_IP}:8080)
+    echo ${curl_return} >> ${FILE_NAME}
+    echo ${curl_return}
 done
 
 sed -i 's/,/./g' ${FILE_NAME}
