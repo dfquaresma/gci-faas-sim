3b29208e2aa894b78feefa7c278580be94c18b01
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-10-09 16:08:28 -0300

    Update build.sh

--- experiment/build.sh
+++ experiment/build.sh
@@ -5,4 +5,7 @@ set -x
 cd ../runtime/thumb-func/
 mvn clean install
 
-cd ../../experiment
+cd ./gci/
+JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64" PATH_TO_GCI="/home/ubuntu/gci-faas-sim/runtime/thumb-func/gci" bash libgcso-entrypoint.sh
+
+cd ../../../experiment
