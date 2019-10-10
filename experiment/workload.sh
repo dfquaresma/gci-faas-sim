4cc88743f4d9a91910bd5955fbba38b6e023e593
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-10-10 13:45:14 -0300

    Update target port environement variable

--- experiment/workload.sh
+++ experiment/workload.sh
@@ -3,7 +3,7 @@ date
 set -x
 
 # To avoid execution without passing environment variables
-if [[ (-z "$NUMBER_OF_REQUESTS") || (-z "$FILE_NAME") || (-z "$FUNCTION_TARGET_IP") || (-z "$RESULTS_PATH") ]];
+if [[ (-z "$NUMBER_OF_REQUESTS") || (-z "$FILE_NAME") || (-z "$FUNCTION_TARGET_IP") || (-z "$RESULTS_PATH") || (-z "$TARGET_PORT") ]];
 then
   echo -e "${RED}MISSING FLAGS IN: workload.sh${NC}"
   exit
@@ -12,7 +12,7 @@ fi
 echo -e "servicetime;latency;status" > ${FILE_NAME}
 for i in `seq 1 ${NUMBER_OF_REQUESTS}`
 do
-    curl_return=$(curl -X GET -s -w ';%{time_total};%{http_code}\n' ${FUNCTION_TARGET_IP}:8080)
+    curl_return=$(curl -X GET -s -w ';%{time_total};%{http_code}\n' ${FUNCTION_TARGET_IP}:${TARGET_PORT})
     echo ${curl_return} >> ${FILE_NAME}
 done
 
