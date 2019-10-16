2fcbad44204a76d3c08e391acf44dbeb44b000a8
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-10-16 11:06:54 -0300

    Update experiments build and setup scripts

--- experiment/build.sh
+++ experiment/build.sh
@@ -2,10 +2,13 @@
 date
 set -x
 
+cd ../gci/
+JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64" PATH_TO_GCI="/home/ubuntu/gci-faas-sim/gci-files" bash libgcso-entrypoint.sh
+
 cd ../runtime/thumb-func/
 mvn clean install
 
-cd ./gci/
-JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64" PATH_TO_GCI="/home/ubuntu/gci-faas-sim/runtime/thumb-func/gci" bash libgcso-entrypoint.sh
+cd ../noop-func/
+mvn clean install
 
 cd ../../../experiment
