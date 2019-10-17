05830c43ba82953941f7c8b1c598ea5571361c63
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-10-17 14:05:04 -0300

    Update build script

--- experiment/build.sh
+++ experiment/build.sh
@@ -2,8 +2,9 @@
 date
 set -x
 
-cd ../gci/
+cd ../gci-files/
 JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64" PATH_TO_GCI="/home/ubuntu/gci-faas-sim/gci-files" bash libgcso-entrypoint.sh
+sudo chmod 775 gci-proxy
 
 cd ../runtime/thumb-func/
 mvn clean install
@@ -11,4 +12,5 @@ mvn clean install
 cd ../noop-func/
 mvn clean install
 
-cd ../../../experiment
+cd ../../experiment
+sudo chmod 775 vegeta
