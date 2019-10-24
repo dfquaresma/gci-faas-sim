2e46fca84ce02722c387c26ffd836c67cf5ff269
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-10-24 17:38:25 -0300

    Move analysis scripts to analysis path

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,8 +1,15 @@
 require(dplyr)
 require(ggplot2)
 require(quantileCI)
+require(base64enc)
 
-stats <- function(df, tag) {
+read.al <- function(path) {
+  df <- read.csv(path, sep=",",header=T, dec=".")
+  df$latency <- df$latency * 1000
+  return(tail(df, -100))
+}
+
+summary_table <- function(df, tag) {
     p50 = quantileCI::quantile_confint_nyblom(df, 0.5)
     p95 = quantileCI::quantile_confint_nyblom(df, 0.95)
     p99 = quantileCI::quantile_confint_nyblom(df, 0.99)
@@ -19,34 +26,6 @@ stats <- function(df, tag) {
     cat("99.999:", signif(p99999, digits = 4), " | ")
     cat("Dist.Tail.:", signif(p99999-p50, digits = 4))
     cat("\n\n")
-}
-
-print_latency_summary_table <- function(first, second, first_tag, second_tag) {
-  first <- filter(first, status == 200)
-  second <- filter(second, status == 200)
-  firstreqs <- first$latency
-  secondreqs <- second$latency
-  stats(firstreqs, paste(first_tag, sep=""))
-  stats(secondreqs, paste(second_tag, sep=""))
-}
-
-read.al <- function(path) {
-  df <- read.csv(path, sep=",",header=T, dec=".")
-  df$latency <- df$latency * 1000
-  return(tail(df, -100))
-}
-
-print_entries_table <- function(i) {
-  path <- "./input-entries/"
-  nogci <- rbind(read.al(paste(path, "nogci", i, ".csv", sep="")))
-  gci <- rbind(read.al(paste(path, "gci", i, ".csv", sep="")))
-  print_summary_table(nogci,  gci)
-}
-
-print_all_entries_table <- function() {
-  for (i in 1:8){
-    print_entries_table(i)
-  }
 }
 
 plot_ecdf = function(GCI, NOGCI, limit) {
@@ -70,12 +49,10 @@ plot_ecdf = function(GCI, NOGCI, limit) {
          horiz = F , inset = c(0.1, 0.1))
 }
 
-#commands to run on R terminal
-#source("analysis.R")
-#gci = filter(rbind(read.al(paste("./", "gci", 1, ".csv", sep=""))), status == 200)
-#nogci = filter(rbind(read.al(paste("./", "nogci", 1, ".csv", sep=""))), status == 200)
-#plot_ecdf(gci,nogci, 130)
-#plot(gci$latency)
-#plot(nogci$latency)
-#hist(gci$latency)
-#hist(nogci$latency)
+get_service_time_column <- function(df) {
+  service_time = c()
+  for (val in df$response_body) {
+    service_time <- c(service_time, as.numeric(rawToChar(base64decode(toString(val)))))
+  }
+  return(service_time)
+}
