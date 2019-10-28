9f2cb1ebaf9f99e81c4715e8046b8905b952077d
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-10-28 14:22:25 -0300

    Update build.sh

--- experiment/build.sh
+++ experiment/build.sh
@@ -3,7 +3,7 @@ date
 set -x
 
 cd ../gci-files/
-JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64" PATH_TO_GCI="/home/ubuntu/gci-faas-sim/gci-files" bash libgcso-entrypoint.sh
+JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64" PATH_TO_GCI="/home/ubuntu/gci-faas-sim/gci-files" bash libgcso-entrypoint.sh
 sudo chmod 775 gci-proxy
 
 cd ../runtime/thumb-func/
