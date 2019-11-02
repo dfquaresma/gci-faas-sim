8a404a8b144a62be982b7e18fae91ef5668c3bfa
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-01 21:45:34 -0300

    Update workload.go file

--- experiment/workload.go
+++ experiment/workload.go
@@ -25,8 +25,7 @@ const (
 	proxyCoreSet     = "taskset 0x2 nice -20 "
 	heapSize         = "-Xms512m -Xmx512m -XX:MaxPermSize=128m"
 	proxyYgen        = "--ygen=419430400 "
-	awsJvmFlags      = "-XX:MaxHeapSize=445645k -XX:MaxMetaspaceSize=52429k -XX:ReservedCodeCacheSize=26214k -Xshare:on 
-	-XX:-TieredCompilation -XX:+UseSerialGC -Djava.net.preferIPv4Stack=true "
+	awsJvmFlags      = "-XX:MaxHeapSize=445645k -XX:MaxMetaspaceSize=52429k -XX:ReservedCodeCacheSize=26214k -Xshare:on -XX:-TieredCompilation -XX:+UseSerialGC -Djava.net.preferIPv4Stack=true "
 	noGcijavaGCFlags = "-server " + heapSize + "-XX:NewRatio=1 " + awsJvmFlags
 	gcijavaGCFlags   = "-server " + heapSize + "-XX:NewRatio=1 " + awsJvmFlags "-XX:NewSize=461m -XX:MaxNewSize=461m " // 460.8 is 90% of 512, so we use 461
 	proxyFlags       = "--port=8080 --target=127.0.0.1:8082 --gci_target=127.0.0.1:8500 " + proxyYgen
