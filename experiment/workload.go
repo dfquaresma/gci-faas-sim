073c95b249ddde42a8cc8917ace16657612dcf56
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-01 21:46:32 -0300

    Update workload.go file

--- experiment/workload.go
+++ experiment/workload.go
@@ -27,7 +27,7 @@ const (
 	proxyYgen        = "--ygen=419430400 "
 	awsJvmFlags      = "-XX:MaxHeapSize=445645k -XX:MaxMetaspaceSize=52429k -XX:ReservedCodeCacheSize=26214k -Xshare:on -XX:-TieredCompilation -XX:+UseSerialGC -Djava.net.preferIPv4Stack=true "
 	noGcijavaGCFlags = "-server " + heapSize + "-XX:NewRatio=1 " + awsJvmFlags
-	gcijavaGCFlags   = "-server " + heapSize + "-XX:NewRatio=1 " + awsJvmFlags "-XX:NewSize=461m -XX:MaxNewSize=461m " // 460.8 is 90% of 512, so we use 461
+	gcijavaGCFlags   = "-server " + heapSize + "-XX:NewRatio=1 " + awsJvmFlags + "-XX:NewSize=461m -XX:MaxNewSize=461m " // 460.8 is 90% of 512, so we use 461
 	proxyFlags       = "--port=8080 --target=127.0.0.1:8082 --gci_target=127.0.0.1:8500 " + proxyYgen
 	jarPath          = runtimePath + "target/thumbnailator-server-maven-0.0.1-SNAPSHOT.jar "
 	funcName         = "thumb"
