9f87a2b839550bbed55d57eca66a9a4b7c18ada8
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-11 12:43:16 -0300

    Update image from 1mb to 400kb

--- experiment/run.sh
+++ experiment/run.sh
@@ -18,7 +18,7 @@ echo "ID_RSA_PATH: ${ID_RSA_PATH:=id_rsa}"
 echo "RESULTS_PATH: ${RESULTS_PATH:=/home/ubuntu/gci-faas-sim/experiment/results/}"
 echo "LOCAL_RESULTS_PATH: ${LOCAL_RESULTS_PATH:=/home/davidfq/Desktop/gci-faas-sim/experiment/results/}"
 echo "CD_TO_SCRIPTS_PATH: ${CD_TO_SCRIPTS_PATH:=cd /home/ubuntu/gci-faas-sim/experiment}"
-echo "IMAGE_URL: ${IMAGE_URL:=https://i.imgur.com/BhlDUOR.jpg}" # default image size is 1mb
+echo "IMAGE_URL: ${IMAGE_URL:=https://i0.wp.com/107.6.131.211/wp-content/uploads/2016/01/pics-012.jpg}" # default image size is 408kb
 
 rm -rf ${LOCAL_RESULTS_PATH}
 
