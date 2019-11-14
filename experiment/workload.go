e5abcc264ee8264656c70257afd74cb649217330
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-14 14:09:59 -0300

    Update workload.go

--- experiment/workload.go
+++ experiment/workload.go
@@ -39,12 +39,11 @@ var (
 	target      = flag.String("target", "", "function's ip and port separated as host:port. There's no default value and should not start with http")
 	logPath     = flag.String("logpath", "", "the absolute path to save logs. It has no default value")
 	nReqs       = flag.Int64("nreqs", 10000, "number of requests, default 10000")
-	fileName    = flag.String("filename", "", "results file name. It has no default value")
 	resultsPath = flag.String("resultspath", "", "absolute path for save results made. It has no default value")
 	imageUrl    = flag.String("image_url", "", "Url of the image to be resized. It has no default value")
 
 	// just to post processor feature
-	nFiles          = flag.Int64("nfiles", 1, "number of file to process, default 1")
+	expIds          = flag.String("expids", "1:2", "A lists of expid values separeted by ':', default '1:2'")
 	inputPath       = flag.String("inputpath", "", "the absolute path to load CSV files. It has no default value")
 	asPostProcessor = flag.Bool("aspostprocessor", false, "Whether to use workload just as post processor, default false")
 )
@@ -56,8 +55,11 @@ func main() {
 		if err := checkPostProcessorFlags(); err != nil {
 			log.Fatalf("invalid flags: %v", err)
 		}
-		err := postprocess(*nFiles, *inputPath, *resultsPath)
-		log.Fatal(err)
+		err := postProcess(*expIds, *inputPath)
+		if err != nil {
+			log.Fatalf("Failure to post processing, error: %v", err)
+		}
+		fmt.Println("Post Process finished")
 		os.Exit(0)
 	}
 
@@ -91,7 +93,16 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("SAVING RESULTS...")
-	if err := createCsv(output, *resultsPath, *fileName); err != nil {
+	var fileName string
+	if *useGci {
+		fileName = "gci"
+	} else {
+		fileName = "nogci"
+	}
+	if err := createCsv(output, *resultsPath, fileName); err != nil {
+		log.Fatal(err)
+	}
+	if err := postProcessExpId(fileName, *expId, *resultsPath); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -109,9 +120,6 @@ func checkWorkloadFlags() error {
 	}
 	if *nReqs <= 0 {
 		return fmt.Errorf("nReqs must be bigger than zero. nReqs: %d", *nReqs)
-	}
-	if len(*fileName) == 0 {
-		return fmt.Errorf("fileName cannot be empty")
 	}
 	if _, err := os.Stat(*resultsPath); os.IsNotExist(err) {
 		return fmt.Errorf("resultsPath must exist. resultsPath: %s", *resultsPath)
@@ -217,38 +225,43 @@ func checkPostProcessorFlags() error {
 	if _, err := os.Stat(*inputPath); os.IsNotExist(err) {
 		return fmt.Errorf("inputPath must exist. inputPath: %s", *inputPath)
 	}
-	if _, err := os.Stat(*resultsPath); os.IsNotExist(err) {
-		return fmt.Errorf("resultsPath must exist. resultsPath: %s", *resultsPath)
-	}
-	if *nFiles <= 0 {
-		return fmt.Errorf("nFiles must be bigger than zero. nFiles: %d", *nFiles)
+	s := strings.Split(*expIds, ":")
+	if len(s) <= 1  && s[0] == "" {
+		return fmt.Errorf("expids must be seperate by ':' and not empty. expids: %s", *expIds)
 	}
 	return nil
 }
 
-func postprocess(nfiles int64, inputPath, resultsPath string) error {
-	var i int64
-	for i = 1; i <= nfiles; i++ {
-		var loadedData []string
-		var err error
-		gciFileName := "gci" + strconv.FormatInt(i, 10) + ".csv"
-		loadedData, err = loadData(inputPath + gciFileName)
+func postProcess(expIds, inputPath string) error {
+	expids := strings.Split(expIds, ":")
+	for _, expid := range expids {
+		err := postProcessExpId("gci", expid, inputPath)
 		if err != nil {
 			return err
 		}
-		err = createCsv(loadedData, resultsPath, gciFileName)
+		err = postProcessExpId("nogci", expid, inputPath)
 		if err != nil {
 			return err
 		}
-		noGciFileName := "nogci" + strconv.FormatInt(i, 10) + ".csv"
-		loadedData, err = loadData(inputPath + noGciFileName)
-		if err != nil {
-			return err
-		}
-		err = createCsv(loadedData, resultsPath, noGciFileName)
-		if err != nil {
-			return err
-		}
+	}
+	return nil
+}
+
+func postProcessExpId(fileName, expid, resultsPath string) error {
+	var loadedData []string
+	var err error
+	fileName = fileName + expid + ".csv"
+	loadedData, err = loadData(resultsPath + fileName)
+	if err != nil {
+		return err
+	}
+	truncatedData, err := truncate503Data(loadedData)
+	if err != nil {
+		return err
+	}
+	err = createCsv(truncatedData, resultsPath, "pp-" + fileName)
+	if err != nil {
+		return err
 	}
 	return nil
 }
@@ -271,7 +284,6 @@ func loadData(filaPath string) ([]string, error) {
 }
 
 func createCsv(output []string, resultsPath, fileName string) error {
-	output = adjustData(output)
 	file, err := os.OpenFile(resultsPath+fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
@@ -285,32 +297,71 @@ func createCsv(output []string, resultsPath, fileName string) error {
 	return nil
 }
 
-func adjustData(data []string) []string {
+func truncate503Data(data []string) ([]string, error) {
 	lastSeenIsA503 := false
-	var adjustedData []string
-	for _, row := range data {
+	var truncatedData []string
+	truncatedData = append(truncatedData, data[0]) // add the CSV header
+	for _, row := range data[1:] { // [1:] because we already added the CSV header
 		splitted := strings.Split(row, ",")
+		var err error
+		splitted, err = convertNumericColumnFromNanoToSec(splitted)
+		if err != nil {
+			return nil, err
+		}
+		row = strings.Join(splitted, ",")
 		//id,status,response_time,body,tsbefore,tsafter - to help when iterating slices
 		statusIs503 := splitted[1] == "503" // index 1 is status
 		if lastSeenIsA503 {
 			if statusIs503 {
-				lastIndex := len(adjustedData) - 1
-				last503AData := strings.Split(adjustedData[lastIndex], ",")
+				lastIndex := len(truncatedData) - 1
+				last503AData := strings.Split(truncatedData[lastIndex], ",")
 				last503AData[5] = splitted[5]        // updating tsafter
 				last503AData[3] += ":" + splitted[2] // updating the list of response time at body column
-				adjustedData[lastIndex] = strings.Join(last503AData, ",")
+				truncatedData[lastIndex] = strings.Join(last503AData, ",")
 			} else {
 				lastSeenIsA503 = false
-				adjustedData = append(adjustedData, row)
+				truncatedData = append(truncatedData, row)
 			}
 		} else {
 			if statusIs503 {
 				lastSeenIsA503 = true
 				splitted[3] = splitted[2] // using body column to put a list of response time
-				row = strings.Join(splitted, ",")
 			}
-			adjustedData = append(adjustedData, row)
+			row = strings.Join(splitted, ",")
+			truncatedData = append(truncatedData, row)
 		}
 	}
-	return adjustedData
+	return truncatedData, nil
+}
+
+func convertNumericColumnFromNanoToSec(row []string) ([]string, error) {
+	// Row format: id,status,response_time,body,tsbefore,tsafter
+	tmp, err := convertNumericStringFromNanoToSec("responseTime", row[2])
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing response_time in row (%v): %q", row, err)
+	}
+	row[2] = tmp
+
+	tmp, err = convertNumericStringFromNanoToSec("tsbefore", row[4])
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing tsafter in row (%v): %q", row, err)
+	}
+	row[4] = tmp
+
+	tmp, err = convertNumericStringFromNanoToSec("tsafter", row[5])
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing tsafter in row (%v): %q", row, err)
+	}
+	row[5] = tmp 
+
+	return row, nil
+}
+
+func convertNumericStringFromNanoToSec(desc , s string) (string, error) {
+	tmp, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return "", fmt.Errorf("Error parsing %s of value %s, error: %q", desc, s, err)
+	}
+	s = fmt.Sprintf("%f", tmp / 1000000000)
+	return s, nil
 }
