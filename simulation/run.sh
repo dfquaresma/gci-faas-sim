b7dcb71ce8d436f746da507c29650e309d4b5a47
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-23 12:08:36 -0300

    Update run.sh

--- simulation/run.sh
+++ simulation/run.sh
@@ -11,7 +11,7 @@ echo "INITIAL_EXPID: ${INITIAL_EXPID:=1}"
 echo "NUMBER_OF_EXPERIMENTS: ${NUMBER_OF_EXPERIMENTS:=1}"
 echo "LAMBDA: ${LAMBDA:=150}"
 echo "WARMUP: ${WARMUP:=500}"
-echo "OP: ${OP:=true false}" # to simulate the optimized scheduler 
+echo "SCHEDULERS: ${OP:=0 1 2}"
 echo "FLAGS: ${FLAGS:=pp-gci pp-nogci}"
 echo "NUMBER_OF_INPUTS: ${NUMBER_OF_INPUTS:=8}"
 echo "OUTPUT_PATH: ${OUTPUT_PATH:=/home/davidfq/Desktop/gci-faas-sim/simulation/results/}"
@@ -23,20 +23,14 @@ for expid in `seq ${INITIAL_EXPID} ${NUMBER_OF_EXPERIMENTS}`;
 do
     for flag in ${FLAGS};
     do
-
         echo -e "${YELLOW}CONCATENATING THE INPUTS${NC}"
         inputs="${INPUT_PATH}${flag}1.csv"
-        for id in `seq 2 ${NUMBER_OF_INPUTS}`;
+        for id in `seq 2 ${NUMBER_OF_INPUTS}`; do inputs="${inputs},${INPUT_PATH}${flag}${id}.csv" done;
+        for sched in ${SCHEDULERS};
         do
-            inputs="${inputs},${INPUT_PATH}${flag}${id}.csv"
-        done;
-
-        for op in ${OP};
-        do
+            if [[ "$sched" = "2" && "$flag" = "pp-nogci" ]]; then continue; fi
             echo -e "${RED}RUNNING SIMULATION, OP=${op}, FLAG=${flag}, EXPID=${expid}${NC}"
-            ./simulator -lambda=${LAMBDA} -inputs=${inputs} -output=${OUTPUT_PATH} -optimized=${op} -filename=sim-${flag}${expid} --warmup=${WARMUP}
+            ./simulator -lambda=${LAMBDA} -inputs=${inputs} -output=${OUTPUT_PATH} -scheduler=${sched} -filename=sim-${flag}${expid} --warmup=${WARMUP}
         done;
     done;
 done
-
-
